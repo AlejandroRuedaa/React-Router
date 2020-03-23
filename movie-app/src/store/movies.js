2664738764cf4a40e03185b41f625368d6eba0fc
bli013 <btli@ymail.com>
2020-03-23 16:25:40 -0700

    Movie names changed

--- movie-app/src/store/movies.js
+++ movie-app/src/store/movies.js
@@ -1,19 +1,19 @@
-const ChewBackacka = [
+const Movies = [
   {
       id: 1,
-      name: "Mac & cheese",
+      name: "Mac & cheese The Movie",
       ingredients: "Fusilli pasta, Cheddar cheese, Jack cheese, butter"
   },
   {
       id: 2,
-      name: "PB&J",
+      name: "PB&J The Movie",
       ingredients: "Bread, peanut butter, grape jelly"
   },
   {
       id: 3,
-      name: "Grilled cheese",
+      name: "Grilled Cheese The Movie",
       ingredients: "Bread, Cheddar cheese, Pepper Jack cheese, butter"
   }
 ]
 
-export default ChewBackacka
+export default Movies
